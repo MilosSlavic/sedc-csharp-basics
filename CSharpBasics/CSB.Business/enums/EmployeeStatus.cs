4003022cb8d872735a5bd9e980b0908466707bc2
Milenamaka <milena85.mm@yahoo.com>
2021-05-05 18:26:44 +0200

    added enum

---
+++ CSharpBasics/CSB.Business/enums/EmployeeStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSB.Business.enums
+{
+    public enum EmployeeStatus
+    {
+        Active = 1,
+        Suspended = 2,
+        Retired = 3,
+        Terminated = 4
+    }
+}
