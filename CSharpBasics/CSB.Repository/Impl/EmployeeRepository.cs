36f75b60162a1a087ad150c925c136ad59465882
Excession27 <feboobef27@gmail.com>
2021-05-05 19:48:34 +0200

    Add EmployeeRepository class to repository project

---
+++ CSharpBasics/CSB.Repository/Impl/EmployeeRepository.cs
@@ -0,0 +1,33 @@
+﻿using CSB.Repository.Entities;
+using CSB.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSB.Repository.Impl
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public int Create(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
