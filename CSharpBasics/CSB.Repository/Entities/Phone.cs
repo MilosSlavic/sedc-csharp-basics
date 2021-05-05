3079ba8961e789c05c6a5b0482bd155c3b129a38
duledd <duledd20@gmail.com>
2021-05-05 18:55:05 +0200

    added phone

---
+++ CSharpBasics/CSB.Repository/Entities/Phone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSB.Repository.Entities
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public string Number { get; set; }
+
+        public short Type { get; set; }
+    }
+}
