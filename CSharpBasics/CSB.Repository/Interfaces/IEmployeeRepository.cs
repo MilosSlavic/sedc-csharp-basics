8e28f9aa645f45a87f6c9bf7118dd86e1f32e81b
vladajevtic <vladajevtic07@gmail.com>
2021-05-05 19:34:10 +0200

    Add interface IEmployeeRepository in repository project

---
+++ CSharpBasics/CSB.Repository/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using CSB.Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSB.Repository.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetById(int id);
+
+        int Create(Employee employee);
+
+        bool Update(Employee employee);
+
+        bool Delete(int id);
+    }
+}
