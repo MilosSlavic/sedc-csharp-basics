e15ce7ceb9c7a436d3195f1b4b54207b83332684
MarijaZ83 <mmmaja83@yahoo.com>
2021-05-05 19:17:17 +0200

    added Employee

---
+++ CSharpBasics/CSB.Repository/Entities/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSB.Repository.Entities
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public short Status { get; set; }
+        public short Gender { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        // TODO: Position 
+    }
+}
