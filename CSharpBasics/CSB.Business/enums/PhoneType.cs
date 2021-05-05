feb9c671ce64e38922e5d3a6ce9521dcb596e104
duledd <duledd20@gmail.com>
2021-05-05 18:49:02 +0200

    added PhoneType

---
+++ CSharpBasics/CSB.Business/enums/PhoneType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSB.Business.enums
+{
+    public enum PhoneType
+    {
+        Cell,
+        Home,
+        Work,
+        Temporary
+    }
+}
