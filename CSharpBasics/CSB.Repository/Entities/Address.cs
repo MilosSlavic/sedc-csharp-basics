6ccf675e5cffe9067ecee69a3bc7ea02cecad710
nikoProg <nikoprog@yahoo.com>
2021-05-05 18:42:10 +0200

    added Address entity

---
+++ CSharpBasics/CSB.Repository/Entities/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSB.Repository.Entities
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public string StreetName { get; set; }
+
+        public string StreetNumber { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public int PostalCode { get; set; }
+
+    }
+}
