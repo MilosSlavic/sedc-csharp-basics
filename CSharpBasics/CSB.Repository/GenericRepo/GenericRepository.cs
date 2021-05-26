0fceeb9ff4ffeeb86c7d8bd569c0c63f68735d52
nikoProg <nikoprog@yahoo.com>
2021-05-26 20:51:30 +0200

    Implemented GenericRepository.

--- CSharpBasics/CSB.Repository/GenericRepo/GenericRepository.cs
+++ CSharpBasics/CSB.Repository/GenericRepo/GenericRepository.cs
@@ -3,16 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSB.Repository.Entities;
 
 namespace CSB.Repository.GenericRepo
 {
-    public class GenericRepository : IGenericRepository
+    public abstract class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
         private readonly CbsDbContext dbContext;
 
         public GenericRepository(CbsDbContext dbContext)
         {
             this.dbContext = dbContext;
+        }
+        public T GetById(int id)
+        {
+            return dbContext.Set<T>().SingleOrDefault<T>(x => x.Id == id);
+        }
+
+        public int Create(T item)
+        {
+            dbContext.Set<T>().Add(item);
+            dbContext.SaveChanges();
+            return item.Id;
+        }
+
+        public bool Delete(int id)
+        {
+            var item = GetById(id);
+            if (item == null)
+            {
+                return false;
+            }
+
+            dbContext.Set<T>().Remove(item);
+            dbContext.SaveChanges();
+            return true;
+        }
+
+        public IReadOnlyCollection<T> GetAll()
+        {
+            return dbContext.Set<T>().ToList();
+        }
+
+        public bool Update(T item)
+        {
+            var entry = dbContext.Entry(item);
+            entry.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            dbContext.SaveChanges();
+            return true;
         }
     }
 }
