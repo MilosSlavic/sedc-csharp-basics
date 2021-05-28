87e25551430aa2b54631c2b7209f56f7ea10d986
duledd <duledd20@gmail.com>
2021-05-28 13:35:50 +0200

    Create phone repository

---
+++ CSharpBasics/CSB.Repository/Impl/PhoneRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using CSB.Repository.Entities;
+using CSB.Repository.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSB.Repository.GenericRepo;
+
+namespace CSB.Repository.Impl
+{
+    //public abstract class PhoneRepository<T> : GenericRepository<T> where T : BaseEntity
+    public class PhoneRepository<T> : IPhoneRepository<T>  where T : BaseEntity
+    {
+        private readonly CbsDbContext dbContext;
+
+        public PhoneRepository(CbsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        //public PhoneRepository(CbsDbContext dbContext) : GenericRepository(dbContext);
+
+        private FileDbProvider _fileDb;
+        public PhoneRepository(FileDbProvider fileDb)
+        {
+            _fileDb = fileDb;
+        }
+
+        public Phone GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Create(Phone phone)
+        {
+            var maxId = _fileDb.Phones.Max(x => x.Id);
+            phone.Id = maxId + 1;
+            _fileDb.Phones.Add(phone);
+            _fileDb.Save();
+            return phone.Id;
+        }
+
+        public bool Update(Phone phone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Phone> GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        T IGenericRepository<T>.GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CSharpBasics/CSB.Repository/Interfaces/IPhoneRepository.cs
@@ -0,0 +1,23 @@
+﻿using CSB.Repository.Entities;
+using CSB.Repository.GenericRepo;
+using CSB.Repository.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSB.Repository.Interfaces
+{
+    internal interface IPhoneRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        Phone GetById(int id);
+        int Create(T phone);
+
+        bool Update(T phone);
+
+        bool Delete(int id);
+
+        List<Phone> GetByName(string name);
+    }
+}
