3e53163369faef60c2b6fe0109fa1d7ca376d7a7
idaMilinkovic <idamilinkovic@gmail.com>
2021-05-30 01:01:46 +0200

    Implemented PositionRepository

---
+++ CSharpBasics/CSB.Repository/Impl/PositionRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSB.Repository.GenericRepo;
+using CSB.Repository.Entities;
+using CSB.Repository.Interfaces;
+
+namespace CSB.Repository.Impl
+{
+    public class PositionRepository : GenericRepository<Position>, IPositionRepository
+    {
+        private readonly CbsDbContext dbContext;
+
+        public PositionRepository(CbsDbContext dbContext) : base(dbContext)
+        {
+            
+        }
+
+        public Position GetPosition(int id)
+        {
+            var item = dbContext.Set<Employee>().SingleOrDefault<Employee>(x => x.Id == id);
+            if(item != null)
+            {
+                return item.Position;
+            }
+            return null;
+        }
+    }
+}
