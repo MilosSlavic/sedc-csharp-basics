ed74bce6c50136df437a5ebd3158aefb5addce2c
vladajevtic <vladajevtic07@gmail.com>
2021-05-30 18:09:30 +0200

    Create position service

---
+++ CSharpBasics/CSB.Business/Impl/PositionService.cs
@@ -0,0 +1,32 @@
+﻿using CSB.Business.Exceptions;
+using CSB.Business.Interfaces;
+using CSB.Repository.Entities;
+using CSB.Repository.Interfaces;
+using System;
+
+namespace CSB.Business.Impl
+{
+    internal class PositionService : IPositionService
+    {
+        private readonly IPositionRepository _positionRepository;
+        public PositionService(IPositionRepository positionRepository)
+        {
+            _positionRepository = positionRepository;
+        }
+        public Position GetPositionByEmployeeId(int employeeId)
+        {
+            if (employeeId <= 0)
+            {
+                throw new ArgumentException("wrong Id");
+            }
+
+            var position = _positionRepository.GetPositionByEmployeeId(employeeId);
+            if(position is null)
+            {
+                throw new NotFoundException("no position");
+            }
+
+            return position;
+        }
+    }
+}
---
+++ CSharpBasics/CSB.Business/Interfaces/IPositionService.cs
@@ -0,0 +1,9 @@
+﻿using CSB.Repository.Entities;
+
+namespace CSB.Business.Interfaces
+{
+    public interface IPositionService
+    {
+        Position GetPositionByEmployeeId(int employeeId);
+    }
+}
