9e6bc2febd2c3667797b780e401a35b5e49ff691
MilosSlavic <62957449+MilosSlavic@users.noreply.github.com>
2021-06-09 20:55:22 +0200

    Get employee from GUI

--- CSharpBasics/CSB.Business/BusinessProfile.cs
+++ CSharpBasics/CSB.Business/BusinessProfile.cs
@@ -14,14 +14,12 @@ namespace CSB.Business
     {
         public BusinessProfile()
         {
-            CreateMap<short, EmployeeStatus>();
-            CreateMap<EmployeeStatus, short>();
-            CreateMap<short, Gender>();
-            CreateMap<Gender, short>();
             CreateMap<CreateEmployeeDto, Employee>()
                 .ForMember(dest => dest.Status, opt => opt.MapFrom(_ => (short)EmployeeStatus.Active))
                 .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => (short)src.Gender));
-            CreateMap<Employee, GetEmployeeDto>();
+            CreateMap<Employee, GetEmployeeDto>()
+                .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => (Gender)src.Gender))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => (EmployeeStatus)src.Status));
         }
     }
 }
