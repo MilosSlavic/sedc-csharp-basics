a9825c4fc00a4d42174ab4acafbab97dc14ab063
MilosSlavic <62957449+MilosSlavic@users.noreply.github.com>
2021-06-16 17:56:39 +0200

    Reversed changes

--- CSharpBasics/CSB.Repository/Impl/PositionRepository.cs
+++ CSharpBasics/CSB.Repository/Impl/PositionRepository.cs
@@ -6,23 +6,22 @@ using System.Threading.Tasks;
 using CSB.Repository.GenericRepo;
 using CSB.Repository.Entities;
 using CSB.Repository.Interfaces;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace CSB.Repository.Impl
 {
     public class PositionRepository : GenericRepository<Position>, IPositionRepository
     {
-        private readonly CbsDbContext dbContext;
+        private readonly CbsDbContext _dbContext;
 
-        public List<Position> Positions;
         public PositionRepository(CbsDbContext dbContext) : base(dbContext)
-        { 
+        {
 
         }
 
-        public Task<Position> GetPositionByEmployeeIdAsync(int employeeId)
+        public async Task<Position> GetPositionByEmployeeIdAsync(int employeeId)
         {
-            throw new NotImplementedException();
+            return await _dbContext.Positions.SingleOrDefaultAsync(x => x.Id == employeeId);
         }
     }
 }
