ae4521aa600afced5ff4934c5e526415176b51e4
duledd <duledd20@gmail.com>
2021-06-16 13:48:30 +0200

    Implement Address service #77

---
+++ CSharpBasics/CSB.Business/Interfaces/IAddressService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSB.Repository.Entities;
+using CSB.Repository.GenericRepo;
+
+namespace CSB.Business.Interfaces
+{
+    interface IAddressService
+    {
+        Task<Address> GetAddressesByIdAsync(int employeeId);
+    }
+}
