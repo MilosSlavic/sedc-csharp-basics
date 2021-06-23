7f01d466f93c7ad2ecb3b60df73c43aa6369208d
MarijaZ83 <mmmaja83@yahoo.com>
2021-06-23 20:51:27 +0200

    CSB-58

--- CSharpBasics/CSB.Repository/Impl/PhoneRepository.cs
+++ CSharpBasics/CSB.Repository/Impl/PhoneRepository.cs
@@ -4,6 +4,7 @@ using CSB.Repository.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 using CSB.Repository.GenericRepo;
+using System.Threading.Tasks;
 
 namespace CSB.Repository.Impl
 {
@@ -13,7 +14,32 @@ namespace CSB.Repository.Impl
         {
         }
 
+        public Task<int> CreateAsync(Phone phone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyCollection<Phone>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Phone> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Phone> GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Phone phone)
         {
             throw new NotImplementedException();
         }
