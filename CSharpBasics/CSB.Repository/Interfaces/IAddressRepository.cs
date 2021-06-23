ee5484b3e96935195c67fb1f88fc5fd0a6b35596
MilosSlavic <62957449+MilosSlavic@users.noreply.github.com>
2021-06-23 20:36:51 +0200

    Build fix

--- CSharpBasics/CSB.Repository/Interfaces/IAddressRepository.cs
+++ CSharpBasics/CSB.Repository/Interfaces/IAddressRepository.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CSB.Repository.Entities;
+﻿using CSB.Repository.Entities;
 using CSB.Repository.GenericRepo;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CSB.Repository.Interfaces
 {
     public interface IAddressRepository : IGenericRepository<Address>
     {
         Task<IReadOnlyList<Address>> GetAddressesAsync(int employeeId);
-        Task<Address> GetAddressesByIdAsync(int employeeId);
     }
 }
